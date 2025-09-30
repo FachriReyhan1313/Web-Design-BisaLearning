d5a3150f9253efd1606aa6455454f32e79a6c941
Fachri Reyhan <fahryreyhanantoni@gmail.com>
2025-09-30 21:50:50 +0700

    Delete controller directory

--- controller/generate_hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-$username = "siswa1";
-$passwordPlain = "rahasia123"; // password asli
-$hashedPassword = password_hash($passwordPlain, PASSWORD_BCRYPT);
-
-echo "Username: $username <br>";
-echo "Password asli: $passwordPlain <br>";
-echo "Password hash: $hashedPassword";
